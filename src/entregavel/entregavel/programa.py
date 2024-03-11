8d438baf7dd74895163dcb53777fbfe976d33fd0
Kauagpaiva <skaliburgpc@gmail.com>
2024-03-10 22:41:31 -0300

    adicionando publisher

--- src/entregavel/entregavel/programa.py
+++ src/entregavel/entregavel/programa.py
@@ -1,18 +1,23 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 
 
 class Assinante(Node):
     def __init__(self):
         super().__init__('Assinante')
         self.get_logger().info('Escutando topico')
+
         self._carrinho = self.create_subscription(Odometry, "/carrinho/odometry", self.carrinho, 10)
         self._alvo1 = self.create_subscription(Odometry, "/alvo1/odometry", self.alvo1, 10)
         self._alvo2 = self.create_subscription(Odometry, "/alvo2/odometry", self.alvo2, 10)
         self._alvo4 = self.create_subscription(Odometry, "/alvo4/odometry", self.alvo3, 10)
         self._alvo5 = self.create_subscription(Odometry, "/alvo5/odometry", self.alvo4, 10)
         self._alvo3 = self.create_subscription(Odometry, "/alvo3/odometry", self.alvo5, 10)
+
+        self._publicador = self.create_publisher(Twist, "cmd_vel", 10)
+        self.create_timer(1, self.publicar_velocidade)
        
     def carrinho(self, objeto):
         vel_linear = objeto.twist.linear
@@ -43,6 +48,9 @@ class Assinante(Node):
         vel_linear = objeto.twist.linear
         vel_angular = objeto.twist.angular
         position = objeto.pose
+
+    def atualizar_velocidade(self):
+        pass #Criar uma lógica pra atualizar a velocidade do carrinho
 
 
 def main(args=None):
